cd82a57639b72aa8bb16c0d3c4a3e60319e85bdf
David Kocher <dkocher@iterate.ch>
2026-04-11 23:17:54 +0200

    Add test.

---
+++ cryptomator/src/test/java/ch/cyberduck/core/cryptomator/impl/uvf/DefaultUVFVaultMetadataProviderTest.java
@@ -0,0 +1,36 @@
+package ch.cyberduck.core.cryptomator.impl.uvf;
+
+/*
+ * Copyright (c) 2002-2026 iterate GmbH. All rights reserved.
+ * https://cyberduck.io/
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ */
+
+import ch.cyberduck.core.vault.VaultCredentials;
+
+import org.junit.Test;
+
+import junit.framework.TestCase;
+
+import static org.junit.Assert.assertArrayEquals;
+
+public class DefaultUVFVaultMetadataProviderTest extends TestCase {
+
+    @Test
+    public void testGetMetadata() throws Exception {
+        final DefaultUVFVaultMetadataProvider provider = new DefaultUVFVaultMetadataProvider(new VaultCredentials("test"));
+        final byte[] b1 = provider.decrypt();
+        assertArrayEquals(b1, provider.decrypt());
+        provider.encrypt();
+        assertArrayEquals(b1, provider.decrypt());
+    }
+}
