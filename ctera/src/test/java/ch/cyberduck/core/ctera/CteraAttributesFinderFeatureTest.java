9b5d0aeb25672acc6a8bab746f06f5e4fa95e10e
David Kocher <dkocher@iterate.ch>
2026-05-12 16:40:18 +0200

    Delete assertion on folder.

--- ctera/src/test/java/ch/cyberduck/core/ctera/CteraAttributesFinderFeatureTest.java
+++ ctera/src/test/java/ch/cyberduck/core/ctera/CteraAttributesFinderFeatureTest.java
@@ -135,9 +135,6 @@ public class CteraAttributesFinderFeatureTest extends AbstractCteraTest {
     public void testNoDeleteAcl() throws Exception {
         final Path home = new Path("/ServicesPortal/webdav/Shared With Me/ACL test", EnumSet.of(AbstractPath.Type.directory));
         final Path folder = new Path(home, "NoDelete", EnumSet.of(AbstractPath.Type.directory));
-        final Acl folderAcl = new CteraAttributesFinderFeature(session).find(folder).getAcl();
-        assertEquals(new Acl(new Acl.UserAndRole(new Acl.CanonicalUser(), READPERMISSION)), folderAcl);
-
         final Path file = new Path(folder, "RW no delete.txt", EnumSet.of(AbstractPath.Type.file));
         final Acl fileAcl = new CteraAttributesFinderFeature(session).find(file).getAcl();
         assertEquals(new Acl(new Acl.UserAndRole(new Acl.CanonicalUser(), READPERMISSION)), fileAcl);
